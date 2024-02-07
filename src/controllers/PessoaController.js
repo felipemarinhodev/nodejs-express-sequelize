7bd3d2b1b446caa8112b7bef596acc6ee3aec182
Felipe Marinho <github@felipemarinho.dev>
2024-02-06 17:45:14 -0800

    a3.4 - create route and controller of pessoa

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,11 @@
+class PessoaController {
+  static async getAll(req, res) {
+    try {
+      // TODO: Acessar o modelo.
+    } catch (error) {
+      // erro
+    }
+  }
+}
+
+module.exports = PessoaController;
