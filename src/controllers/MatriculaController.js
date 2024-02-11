4b088c7a6f1282079b8691a0b218a54d52d1fe24
Felipe Marinho <github@felipemarinho.dev>
2024-02-11 11:48:11 -0800

    a4.4 - agregation functions

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -6,6 +6,20 @@ class MatriculaController extends Controller {
   constructor() {
     super(matriculaServices);
   }
+
+  async pegaMatriculasPorEstudante(req, res) {
+    const { estudante_id } = req.params;
+    try {
+      const listaMatriculaPorEstudante = await matriculaServices.pegaEContaRegistros({
+        estudante_id: Number(estudante_id),
+        status: 'matriculado'
+      });
+      return res.status(200).json(listaMatriculaPorEstudante);
+    } catch (error) {
+      return res.status(500).json({ erro: error.message });
+    }
+  }
+
 }
 
 module.exports = MatriculaController;
