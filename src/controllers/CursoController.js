0c1c1fd28e1768282246361ebc0b439fbabd940e
Felipe Marinho <github@felipemarinho.dev>
2024-02-11 10:41:09 -0800

    a4.2 - working with operators

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -1,3 +1,4 @@
+const Op = require('sequelize');
 const CursosServices = require('../services/CursoServices');
 const Controller = require('./Controller');
 
@@ -5,6 +6,20 @@ const cursoServices = new CursosServices();
 class CursoController extends Controller {
   constructor() {
     super(cursoServices);
+  }
+
+  async pegaCursos(req, res) {
+    const { data_inicial, data_final } = req.query;
+
+    const where = {};
+
+    // se existirem os params, criar uma prop {}
+    data_inicial || data_final ? where.data_inicio = {} : null;
+    // se existir data inicial, adiciona a prop gte com o valor
+    data_inicial ? where.data_inicio[Op.gte] = data_inicial : null;
+    // se existir data final, item
+    data_final ? where.data_inicio[Op.lte] = data_final : null;
+
   }
 }
 
